41ea9c16b7062f350d412158cefb4a7a7426fd30
InCogNiTo124 <marijansmetko123@gmail.com>
2019-10-01 15:22:52 +0200

    Fix installation script

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+set -e
+sudo add-apt-repository ppa:kubuntu-ppa/backports
+sudo apt-get update && yes | sudo apt-get upgrade
+sudo apt-get install htop neovim build-essential git tmux
 ln $(pwd)/.bash_aliases ~/.bash_aliases
-source ~/.bashrc
+ln $(pwd)/.bash_functions ~/.bash_functions
+cat >> ~/.bashrc << EOF
 
+if [ -f ~/.bash_functions ]; then
+    . ~/.bash_functions
+fi
+EOF
+source ~/.bashrc
